49192bad0daf2c1a55f5166e02b690128cd12e86
Julius Odunuga <odunugajulius1@gmail.com>
2022-06-21 19:56:16 +0100

    environment variables

--- Project1/julisam/settings.py
+++ Project1/julisam/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fjme^uy#@#r_gr!#5j^#e^3owj0ip242t+^f=3570&s(ca2bcx'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == "True" #since os.getenv returns string
 
 ALLOWED_HOSTS = []
 
